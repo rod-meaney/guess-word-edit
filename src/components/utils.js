ad061d5e317a142787041b772cba06238d6daebb
rodm <rod.meaney@gmail.com>
2020-04-21 10:06:30 +1000

    now pointing to readme instead of source code directly

--- src/components/utils.js
+++ src/components/utils.js
@@ -9,7 +9,7 @@ export function footer(){
       <p>
         <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/rodkm/"><Image style={{width:24}} src={linkedin} /> Developer</a>
         {" | "}
-        <a rel="noopener noreferrer" target="_blank" href="https://github.com/rod-meaney/what-word-is-that"><Image style={{width:24}} src={github} /> Code</a>
+        <a rel="noopener noreferrer" target="_blank" href="https://github.com/rod-meaney/what-word-is-that/blob/master/README.md"><Image style={{width:24}} src={github} /> Code</a>
         {" | "}
         <a rel="noopener noreferrer" target="_blank" href="https://github.com/rod-meaney/what-word-is-that/issues"><Image style={{width:24}} src={github} /> Issues</a>
       </p>
